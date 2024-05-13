447a18406dabe603648edc9a1b6c9babb13cf4c3
hzee97 <janghyeonji0830@naver.com>
2024-05-13 22:30:37 +0900

    백준 2470 두 용액

---
+++ 이분탐색/두 용액.py
@@ -0,0 +1,32 @@
+# keypoint : 이분탐색, 두 포인터
+
+import sys
+input=sys.stdin.readline
+
+n=int(input())
+info=list(map(int,input().split()))
+
+info.sort()   # 정렬
+
+start=0
+end=n-1
+val=sys.maxsize   # 기준값
+
+result1=0
+result2=0
+while start<end:
+    mix=info[start]+info[end]
+
+    if abs(mix)<val:
+        val=abs(mix)
+        result1=info[start]
+        result2=info[end]
+
+    if mix<0:
+        start+=1
+    elif mix>0:
+        end-=1
+    else:
+        break
+
+print(result1,result2)
